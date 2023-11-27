2c6ec56398fa12617086b8407c5bc5d6010cd8f1
Administrator <Administrator@BLRTRAINING1.COM>
2023-11-27 15:19:00 +0530

    Card and counter is updated according to the bootstrap css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ const Card = (props) => {
   const [cardSelected, setCardSelected] = useState(false)
   
   const getCardClass = () => {
-    let cardClass = cardSelected === true ? "card selected" : "card deselected";
+    let cardClass = cardSelected === true ? "selected" : "deselected";
     return cardClass
   }
 
@@ -16,18 +16,18 @@ const Card = (props) => {
   };
 
   return (
-    <div className = {`${getCardClass()} `} onClick={() => setCardSelected(!cardSelected)}>
-      <div>
+    <div className = {`${getCardClass()} card flex-row items-align-center justify-content-between p-3 mb-4 shadow-sm border-lg rounded-4`} onClick={() => setCardSelected(!cardSelected)}>
+      <div className='w-50'>
         <h4 className='m-0'> {props.productDetail.name} </h4>
         <p className='m-0'>{props.productDetail.desc}</p>
       </div>
-      <div className={getOverlayState()}>
-        <Counter 
-          product = {props.productDetail}
-          onIncrement={props.onQuantityIncreased}
-          onDecrement={props.onQuantityDecreased} 
-          />
-      </div>
+
+      {cardSelected && <Counter 
+        product = {props.productDetail}
+        onIncrement={props.onQuantityIncreased}
+        onDecrement={props.onQuantityDecreased} 
+      />}
+
     </div>
   )
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,16 +2,15 @@ import "../styles/counterStyles.css"
 
 export default function Counter(props) {
     return (
-      <div className="counter">
-        Quantity &nbsp;
-        <span className={props.product.quantity === 0 ? "badge badge-warning" : "badge badge-primary"}>
+      <div className="counter d-flex flex-nowrap align-items-center justify-content-end">
+        <div className="mx-1">Quantity:</div>  
+        <div className={`${props.product.quantity === 0 ? "bg-warning text-black" : "bg-primary"} badge mx-1 p-2`}>
           {props.product.quantity === 0 ? "Zero": props.product.quantity}
-        </span>
-        &nbsp;
-        <button className="btn btn-secondary" onClick={props.onIncrement}>
+        </div>
+        <button className="btn btn-secondary mx-1" onClick={props.onIncrement}>
           +
         </button>
-        <button className="btn btn-secondary" onClick={props.onDecrement}>
+        <button className="btn btn-secondary mx-1" onClick={props.onDecrement}>
           -
         </button>
       </div>
